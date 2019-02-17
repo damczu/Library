4837685fcaf0dbae6ced1b9053a2110fc3092fb2
Marcin Iskra <dunravel@gmail.com>
2019-02-17 14:43:22 +0100

    Added Getter Setter annotations to Person

--- domain/src/main/java/pl/sda/domain/model/Person.java
+++ domain/src/main/java/pl/sda/domain/model/Person.java
@@ -1,9 +1,14 @@
 package pl.sda.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Persons")
+@Getter
+@Setter
 public class Person {
     @Id
     @GeneratedValue
