a6d66b7ef1547747c584bd5792140f91a2d0ec63
Marcin Iskra <dunravel@gmail.com>
2019-02-17 15:09:50 +0100

    Created Book entity

---
+++ domain/src/main/java/pl/sda/domain/model/Book.java
@@ -0,0 +1,26 @@
+package pl.sda.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Books")
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue
+    @Column(name = "BOK_Id")
+    private int id;
+    @Column(name = "BOK_Title")
+    private String title;
+    @Column(name = "Bok_IsbnNumber")
+    private String isbnNumber;
+    @Column(name = "Bok_ReleaseDate")
+    private LocalDate releaseDate;
+    @Column(name = "Bok_Summary")
+    private String summary;
+}
