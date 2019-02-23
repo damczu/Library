2911ac95d3dcbd139f98fb57601c8a79332bbf22
Marcin Iskra <dunravel@gmail.com>
2019-02-23 09:44:53 +0100

    Added CRUD to Book repository

--- persistance/src/main/java/pl/sda/persistance/repository/BookRepository.java
+++ persistance/src/main/java/pl/sda/persistance/repository/BookRepository.java
@@ -3,17 +3,31 @@ package pl.sda.persistance.repository;
 import org.hibernate.Session;
 import pl.sda.domain.model.Book;
 
+import java.util.List;
+
 public class BookRepository {
     private final Session session;
-    private final Book book;
 
     public BookRepository(Session session, Book book){
         this.session = session;
-        this.book = book;
     }
 
-    public Integer update(){
+    public Integer update(Book book){
         return(Integer) session.save(book);
+    }
+
+    public Book find(int bookId){
+        return session.find(Book.class, bookId);
+    }
+
+    public void delete(Book book){
+        session.delete(book);
+    }
+
+    public List<Book> findAll(){
+        return session
+                .createQuery("FROM Books")
+                .list();
     }
 
 }
