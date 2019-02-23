22ef8eddf13d9346b1c7c41b8ea31bd6d3cf1dec
Marcin Iskra <dunravel@gmail.com>
2019-02-23 10:47:09 +0100

    Implemented addUser in UserService

--- service/src/main/java/pl/sda/service/UserService.java
+++ service/src/main/java/pl/sda/service/UserService.java
@@ -5,17 +5,27 @@ import pl.sda.domain.model.User;
 import pl.sda.persistance.hibernate.HibernateUtil;
 import pl.sda.persistance.repository.UserRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class UserService {
 
-    public void addUser(String login, char[] password){
+    public Integer addUser(String login, char[] password){
         User user = new User();
         user.setLogin(login);
         user.setPassword(password);
+        user.setCreateDate(LocalDateTime.now());
 
         Session session = HibernateUtil
                 .getSessionFactory()
                 .openSession();
         UserRepository userRepository = new UserRepository(session);
-        userRepository.save(user);
+
+        Integer result =  userRepository.save(user);
+
+        session.close();
+        HibernateUtil.shutdown();
+
+        return result;
     }
 }
