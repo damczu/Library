8fead5b63502e5a7f3d181de21098fdd93f383cd
Marcin Iskra <dunravel@gmail.com>
2019-02-23 12:18:21 +0100

    Added option to detect existing e-mail address

--- persistance/src/main/java/pl/sda/persistance/repository/PersonRepository.java
+++ persistance/src/main/java/pl/sda/persistance/repository/PersonRepository.java
@@ -1,18 +1,25 @@
 package pl.sda.persistance.repository;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import pl.sda.domain.model.Person;
+import pl.sda.persistance.hibernate.HibernateUtil;
+
+import java.util.List;
 
 public class PersonRepository {
 
     private final Session session;
 
-    public PersonRepository(Session session) {
-        this.session = session;
+    public PersonRepository() {
+        this.session = HibernateUtil.getSession();
     }
 
     public Integer save(Person person){
-        return (Integer) session.save(person);
+        Transaction tx = session.beginTransaction();
+        Integer resultId = (Integer) session.save(person);
+        tx.commit();
+        return resultId;
     }
 
     public Person findByID(Integer personId) {
@@ -20,10 +27,20 @@ public class PersonRepository {
     }
 
     public void update(Person person) {
+        Transaction tx = session.beginTransaction();
         session.update(person);
+        tx.commit();
     }
 
     public void delete(Person person) {
+        Transaction tx = session.beginTransaction();
         session.delete(person);
+        tx.commit();
+    }
+
+    public List<Person> findByEmail(String email) {
+
+        return session.createQuery("FROM Person WHERE PER_Email = '" + email + "'").list();
+
     }
 }
--- service/src/main/java/pl/sda/service/PersonService.java
+++ service/src/main/java/pl/sda/service/PersonService.java
@@ -3,6 +3,8 @@ package pl.sda.service;
 import pl.sda.domain.model.Person;
 import pl.sda.persistance.repository.PersonRepository;
 
+import java.util.List;
+
 public class PersonService {
 
     private final PersonRepository personRepository;
@@ -12,16 +14,18 @@ public class PersonService {
     }
 
     public Integer addPerson(String firstName, String lastName, String phone, String email){
-        Person person = new Person();
-        person.setFirstName(firstName);
-        person.setLastName(lastName);
-        person.setPhone(phone);
-        person.setEmail(email);
+        List<Person> personsWithSameEmail = personRepository.findByEmail(email);
+        if(personsWithSameEmail.size() == 0) {
+            Person person = new Person();
+            person.setFirstName(firstName);
+            person.setLastName(lastName);
+            person.setPhone(phone);
+            person.setEmail(email);
 
-        PersonRepository personRepository = new PersonRepository();
-
-        Integer save = personRepository.save(person);
-
-        return save;
+            PersonRepository personRepository = new PersonRepository();
+            Integer save = personRepository.save(person);
+            return save;
+        }
+        return personsWithSameEmail.get(0).getId();
     }
 }
