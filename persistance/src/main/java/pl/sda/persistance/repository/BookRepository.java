18465a72086ec7b0c63f6c35c1b2f9f82ecd7b6d
Marcin Iskra <dunravel@gmail.com>
2019-02-23 13:44:58 +0100

    Updated Book repository

--- persistance/src/main/java/pl/sda/persistance/repository/BookRepository.java
+++ persistance/src/main/java/pl/sda/persistance/repository/BookRepository.java
@@ -1,19 +1,24 @@
 package pl.sda.persistance.repository;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import pl.sda.domain.model.Book;
+import pl.sda.persistance.hibernate.HibernateUtil;
 
 import java.util.List;
 
 public class BookRepository {
     private final Session session;
 
-    public BookRepository(Session session, Book book){
-        this.session = session;
+    public BookRepository(Session session){
+        this.session = HibernateUtil.getSession();
     }
 
     public Integer update(Book book){
-        return(Integer) session.save(book);
+        Transaction tx = session.beginTransaction();
+        Integer bookId = (Integer) session.save(book);
+        tx.commit();
+        return bookId;
     }
 
     public Book find(int bookId){
@@ -21,7 +26,9 @@ public class BookRepository {
     }
 
     public void delete(Book book){
+        Transaction tx = session.beginTransaction();
         session.delete(book);
+        tx.commit();
     }
 
     public List<Book> findAll(){
