ef665b6a4946697f64cbb40c87b556b6c60c29f7
Marcin Iskra <dunravel@gmail.com>
2019-02-23 12:52:03 +0100

    Added UserAddDto

---
+++ domain/src/main/java/pl/sda/domain/model/dto/UserAddDto.java
@@ -0,0 +1,10 @@
+package pl.sda.domain.model.dto;
+
+public class UserAddDto {
+    public String login;
+    public char[] password;
+    public String firstName;
+    public String lastName;
+    public String phone;
+    public String email;
+}
